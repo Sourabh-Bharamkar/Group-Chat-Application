82d7cf9deb90b316b01aa2381a99bdf0ccff6987
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-04-16 15:17:03 +0530

    CORS configuration & 404 error functionality added

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const app = express();
 const sequelize = require('./util/database');
 const bodyParser=require('body-parser')
 const path=require('path')
+const cors=require('cors')
 
+const pageNotFoundMiddleware=require('./middleware/404')
 const userRoutes=require('./routes/user')
+
+app.use(cors({
+    origin:"*",
+  
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -14,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(userRoutes)
 
+app.use(pageNotFoundMiddleware)
 sequelize.sync()
     .then(() => {
         app.listen(3000)
